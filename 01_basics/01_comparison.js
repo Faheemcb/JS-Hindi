1424bc0099754c1ff63b1f6e978381752f5ce98a
Mohammed Faheem <116952615+Faheemcb@users.noreply.github.com>
2023-03-17 17:14:16 +0000

    memory

--- 01_basics/01_comparison.js
+++ 01_basics/01_comparison.js
@@ -8,3 +8,30 @@ console.log(null >= 0);  //true
 // == & > works slightly diiferent
 
 // === checks datatype
+
+// ***************  Memory ************
+
+//Stack (Primitive) , Heap (Non Primitive)
+
+let busName = "KMT"
+let anotherBus = busName
+anotherBus = "Johny's"
+
+console.log(busName);
+console.log(anotherBus);
+
+//Gets a copy for stack
+
+let userOne = {
+    email : "user@google.com",
+    upi : "keef@ybl"
+}
+
+let userTwo = userOne //Getting a copy
+
+userTwo.email = "faheem@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+//Gets reference for heap
