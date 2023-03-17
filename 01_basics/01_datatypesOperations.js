e6da658f30406118a9719232efdcfde5011b73dc
Mohammed Faheem <116952615+Faheemcb@users.noreply.github.com>
2023-03-17 11:44:33 +0000

    conversion

---
+++ 01_basics/01_datatypesOperations.js
@@ -0,0 +1,28 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33fgfg" => NaN
+// true => 1 , false =>0
+// null => 0
+//undefined => NaN
+
+let isLoggedIn = "ds";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+//"dfdg" => true 
+
+let someNumber = 33
+ 
+let stringNumber = String(someNumber)
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //string
