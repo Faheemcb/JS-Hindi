64cbfa50ebf0c9b5b58465e8a3cfca7ed7b0f05a
Mohammed Faheem <116952615+Faheemcb@users.noreply.github.com>
2023-03-17 05:12:07 +0000

    day1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14785;
+let accoountEmail = "mohammed@gmail.com";
+var accountPassword = "12345";
+accountCity  = "Kerala"
+let accountState
+// accountId = 2  Not allowed
+
+accountEmail = "ddhh";
+accountPassword = "55757"
+accountCity = "Goa";
+
+
+console.log(accountId);
+
+/*
+Prefered not to use var
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
