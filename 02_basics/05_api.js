482a3c81f9d1e2559a9cd7b934a86c6e030e273e
Mohammed Faheem <116952615+Faheemcb@users.noreply.github.com>
2023-07-05 16:11:54 +0000

    API and JSON

---
+++ 02_basics/05_api.js
@@ -0,0 +1,19 @@
+// """"API"""""'
+// """VALUE COMES AS JSON"
+ 
+//                                     J
+//     "name": "Mohammed Faheem",      S
+//     "coursename": "MEAN",           O
+//     "price" : "79000"               N
+// }
+
+// SOMETIMES API COMES IN ARRAY FORMAT
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
+// FAMOUS API "https://randomuser.me/"
+// to format use https://jsonformatter.org/
