6933069506061027c33a7dfa276203821c18c49c
Mohammed Faheem <116952615+Faheemcb@users.noreply.github.com>
2023-07-09 12:17:56 +0000

    first impression on hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,11 +5,11 @@
 
 // GLOBAL SCOPE
 let a = 300
-if (true) {
+if (true) { 
     let a = 10  // ReferenceError: a is not defined      // MEMORY IS INSIDE THIS SCOPE "ONLY"
     const b = 20 // ReferenceError: B is not defined
     // var c = 30  // 30  delacare var or not same thing happens
- console.log("INNER " , a);
+//  console.log("INNER " , a);
     // INSIDE IS BLOCK SCOPE
 }
 
@@ -24,10 +24,43 @@ if (true) {
 // """"' NESTED SCOPE """"""''''
 function one() {
     const username = "faheem"
-    function two() {
-        const website = "youtbue" 
-        console.log(username);                                                                                                               
+    function two() { // function inside function
+        const website = "youtube" 
+        console.log(username);
+                                                                                                                    
     }
-    console.log();
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if (true) {
+    const username = "Faheem"
+    if(username === "Faheem"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website); will not print because it's outside the scope
 }
 
+// console.log(username); will not print because the scope is not here
+
+// +++++++++++++++++++ interesting ++++++++++++++
+// HOISTING
+
+
+console.log(addone(5)) //it logs
+
+function addone(num){
+    return num+1
+
+}
+// addone(5)
+
+//   Called Expression (Example of Hoisting)
+console.log(addTwo(5)); // it doesnt logs because of defining function in expression
+const addTwo = function(num){
+    return num + 2 
+
+}
